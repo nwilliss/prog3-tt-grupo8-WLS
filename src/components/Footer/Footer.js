a90ee78adaf3e24a2391e900faf7cc646b5a303f
nwilliss <nwillis@udesa.edu.ar>
2024-09-09 17:24:17 -0300

    arreglo colores

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer =()=>{
     return(
         <footer>
             <div className="footer-content">
-                <p>Copyright © 2024 Pandafilm. Todos los derechos reservados.</p>
+                <p>Copyright © 2024 wlsmovies. Todos los derechos reservados.</p>
             <ul className="team">
                 <li> © Victoria Laplume</li>
                 <li> Mia Schwarzmaier</li>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,12 @@ import SearchForm from '../components/SearchForm/SearchForm'
 const Home = () => {
   return (
     <>
-    <h1>Home</h1>
+   
+    <h1 >Home</h1>
     <main>
      <SearchForm />
     </main>
+
   </>
   )
 }
