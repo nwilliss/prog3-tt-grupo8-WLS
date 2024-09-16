a6194c6b9c31d3092b3287cb6d430ab0eddb5e18
Victoria Laplume <victorialaplume@MacBook-Air-de-Victoria.local>
2024-09-16 10:51:32 -0300

    estilo

--- src/pages/Favoritos.js
+++ src/pages/Favoritos.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Favoritos = () => {
   return (
-   <h1>estos son tus favoritos</h1>
+   <h1>Estos son tus favoritos</h1>
 
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
    
-    <h1 >Home</h1>
+    {/* <h1 >Home</h1> */}
     <main>
      <SearchForm />
      <Movie/>
