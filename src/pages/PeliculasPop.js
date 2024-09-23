bb95a6f5e59cb3461896203f89e524e8e5fdd8b0
miaschwarzmaier <mschwarzmaier@udesa.edu.ar>
2024-09-23 18:08:23 -0300

    loader

--- src/pages/PeliculasPop.js
+++ src/pages/PeliculasPop.js
@@ -5,34 +5,49 @@ import "../index.css"
 import MovieCard from '../components/MovieCard/MovieCard'
 
 class PeliculasPop extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            peliculas:[], 
+        this.state = {
+            peliculas: [],
+            isLoading: true,
+
         }
     }
 
 
-    componentDidMount(){
+    componentDidMount() {
+        this.setState({
+            isLoading: true
+        })
         fetch("https://api.themoviedb.org/3/movie/popular?api_key=c8a7b4c53789e8169ffd16fbbfe4254f&language=en-US&page=1")
-        .then((res)=> res.json() )
-        .then((data)=> this.setState({peliculas:data.results}))
+            .then((res) => res.json())
+            .then((data) => this.setState({ peliculas: data.results }))
         //catch
+        this.setState({
+            isLoading: false
+        })
+
     }
 
-    render(){
+    render() {
 
-        return(
+        return (
             <>
-            <section className='peliFav'>
-            {this.state.peliculas.map((oneMovie, idx) => <MovieCard key={oneMovie.name + idx} datosPelis={oneMovie} />)}
-            </section>
+                <section className='peliFav'>
+                    {this.state.peliculas.lenght === 0 && this.state.isLoding ?
+                        <p>Loading...</p>
+                        :
+                        this.state.peliculas.lenght === 0 && !this.state.isLoding ?
+                            <p>No hay resultados</p> :
+                            this.state.peliculas.map((oneMovie, idx) => <MovieCard key={oneMovie.name + idx} datosPelis={oneMovie} />)}
+                </section>
 
             </>
 
 
-        
-     )
+
+
+        )
 
     }
 }
--- src/pages/PeliculasTop.js
+++ src/pages/PeliculasTop.js
@@ -5,36 +5,50 @@ import "../index.css"
 
 
 class PeliculasTop extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            peliculas:[], 
+        this.state = {
+            peliculas: [],
+            isLoading: true,
+
         }
     }
 
 
-    componentDidMount(){
-        
+    componentDidMount() {
+        this.setState({
+            isLoading: true
+        })
         fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=c8a7b4c53789e8169ffd16fbbfe4254f")
-        .then((res)=> res.json() )
-        .then((data)=> this.setState({peliculas:data.results}))
+            .then((res) => res.json())
+            .then((data) => this.setState({ peliculas: data.results }))
         //catch
-        
+        this.setState({
+            isLoading: false
+        })
+
+
     }
 
-    render(){
+    render() {
 
-        return(
+        return (
             <>
-              <section className='peliFav'>
-            {this.state.peliculas.map((oneMovie, idx) => <MovieCard key={oneMovie.name + idx} datosPelis={oneMovie} />)}
-            </section>
+                <section className='peliFav'>
+                    {this.state.peliculas.lenght === 0 && this.state.isLoding ?
+                        <p>Loading...</p>
+                        :
+                        this.state.peliculas.lenght === 0 && !this.state.isLoding ?
+                            <p>No hay resultados</p> :
+                            this.state.peliculas.map((oneMovie, idx) => <MovieCard key={oneMovie.name + idx} datosPelis={oneMovie} />)}
+                </section>
 
             </>
 
 
-        
-     )
+
+
+        )
 
     }
 }
